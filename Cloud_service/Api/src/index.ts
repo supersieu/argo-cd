2eb0cd3258ce9ee41f351567b24875c4a08175d1
NGUYEN Ngoc Dang <nguyen.ngocdang1996@gmail.com>
2023-04-26 23:35:22 +0200

    Update index.ts

--- Cloud_service/Api/src/index.ts
+++ Cloud_service/Api/src/index.ts
@@ -24,7 +24,7 @@ wss.on('connection', (ws) => {
         fsm.startFsm(); 
     });
 
-    ws.send('Welcome to the server! argocd');
+    ws.send('Welcome to the server! argocd----');
 });
 
 // let id = 9445166;
