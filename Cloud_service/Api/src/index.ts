4a5ae61eaf7c74ae9a05d136d20d755fc40cfbcd
NGUYEN Ngoc Dang <nguyen.ngocdang1996@gmail.com>
2023-04-27 09:12:26 +0200

    fix123

--- Cloud_service/Api/src/index.ts
+++ Cloud_service/Api/src/index.ts
@@ -24,7 +24,7 @@ wss.on('connection', (ws) => {
         fsm.startFsm(); 
     });
 
-    ws.send('Welcome to the server!--------argocd ta grand mere!!!!!!!!!!!!!!!');
+    ws.send('Welcome to the server!--------argocd ta grand merefezezfezf!!!!!!!!!!!!!!!');
 });
 
 // let id = 9445166;
